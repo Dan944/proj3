8303c3328603405200853c523c49d86a23c1ce4c
longgeY <96642540+longgeY@users.noreply.github.com>
2024-03-25 02:51:16 -0400

    Update myserver.cpp

--- myserver.cpp
+++ myserver.cpp
@@ -16,6 +16,8 @@
 #include <sstream>
 #include <locale>
 #include <cctype>
+#include "User.h"
+#include "System.h"
 
 using namespace std;
 vector<int> states(100, 0);
@@ -41,216 +43,217 @@ void print_fd_set(const fd_set *set, int ccc) {
     }
 }
 
-static inline void rtrim(std::string &s) {
-    s.erase(std::find_if(s.rbegin(), s.rend(), [](unsigned char ch) {
-        return !std::isspace(ch);
-    }).base(), s.end());
-}
+// static inline void rtrim(std::string &s) {
+//     s.erase(std::find_if(s.rbegin(), s.rend(), [](unsigned char ch) {
+//         return !std::isspace(ch);
+//     }).base(), s.end());
+// }
 
 void writeLine(int socketId, const string line) {
     string temp = line + '\n';
     write(socketId, temp.c_str(), temp.size());
 }
 
-struct User {
-    string username; 
-    string password;
-    string information; 
-    vector<string> blocked_names;
-    bool quiet; /* whether this user is quiet*/
-    bool login; /* whether this user is online*/
-    string observeId;
-    string playId;
-    int sockId;
-	float rating;
-	int win;
-	int loss;
-	int id;
-	int cmd;
+// struct User {
+//     string username; 
+//     string password;
+//     string information; 
+//     vector<string> blocked_names;
+//     bool quiet; /* whether this user is quiet*/
+//     bool login; /* whether this user is online*/
+//     string observeId;
+//     string playId;
+//     int sockId;
+// 	float rating;
+// 	int win;
+// 	int loss;
+// 	int id;
+// 	int cmd;
 
-    User(string username, string password) {
-        this->username = username;
-        this->password = password;
-        information = username;
-        quiet = false;
-        login = false;
-        sockId = -1;
-		cmd = 0 ;
-		win = 0;
-		loss = 0;
-		rating = 0;
+//     User(string username, string password) {
+//         this->username = username;
+//         this->password = password;
+//         information = username;
+//         quiet = false;
+//         login = false;
+//         sockId = -1;
+// 		cmd = 0 ;
+// 		win = 0;
+// 		loss = 0;
+// 		rating = 0;
 
-    }
-	User() {
-        this->username = username;
-        this->password = password;
-        information = username;
-        quiet = false;
-        login = false;
-        sockId = -1;
-		cmd = 0;
-		win = 0;
-		loss = 0;
-		rating = 0;
-    }
-	void logout() {
-		login = false;
-		cmd = 0;
-		sockId = -1;
-		//game-related should be dealed here
-	}
-	void writef(string buf) {
-		char outbuf[1024];
-		sprintf(outbuf, "<%s: %i> %s", username.c_str(), cmd, buf.c_str());
-		cmd++;
-		// Ensure num is the length of outbuf, not the result of the read call
-		write(sockId, outbuf, strlen(outbuf)); 
-	}
-};
-struct System {
-    vector<User*> allUsers;
-	vector<User*> onlineUsers;
-	vector<string> getAllUsers() {
-        vector<string> result;
-        for (User *u : allUsers) {
-            result.push_back(u->username);
-        }
-        return result;
-    }
-	User* findUser(string username) {
-		rtrim(username);
-        for (User *u : allUsers) {
-            if (u->username == username) {
-				cout << "finded\n";
-                return u;
-            }
-        }
-        return nullptr;
-    }
-	User* findUserFd(const int fd) {
-        for (User *u : allUsers) {
-            if (u->sockId == fd) {
-                return u;
-            }
-        }
-        return nullptr;
-    }
-	void init() {
-		System::load_user();
-	}
-	void onlineUpdate(){
-		onlineUsers.clear();
-		for (User* u : allUsers) {
-			if (u->login) { // If the user is marked as online
-				onlineUsers.push_back(u);
-			}
-		}
-	}
-	void who(int fd) {
-		onlineUpdate();
-		char out1[100];
-		sprintf(out1,"Total %i user(s) online:\n", onlineUsers.size());
-		string out2;
-		for (User* u : onlineUsers) {
-			out2 += u->username;
-			out2 += " ";
-		}
-		write(fd, out1, strlen(out1));
-		writeLine(fd, out2);
-	}
-	void load_user() {
-		std::string filePath = "data/user"; // Adjust the path as necessary
-		std::ifstream file(filePath);
-		if (!file.is_open()) {
-			std::cerr << "Failed to open user file." << std::endl;
-		}
-		std::string line;
-		std::getline(file, line);
-		while (std::getline(file, line)) {
-			// user,password,id,info,rating,win,loses,quiet,block,online
-			int count = 0;
-			User *user = new User();
-			std::stringstream ss(line);
-			std::string item;
-			std::vector<std::string> tokens;
-			while (std::getline(ss, item, ',')) {
-				tokens.push_back(item);
-			}
-			user->username = tokens[0];
-			user->password = tokens[1];
-			user->id = std::stoi(tokens[2]);
-			user->information = tokens[3];
-			user->rating = std::stof(tokens[4]);
-			user->win = std::stoi(tokens[5]);
-			user->loss = std::stoi(tokens[6]);
-			user->quiet = std::stoi(tokens[7]);		
-			std::istringstream blockStream(tokens[8]); // Use tokens[8] to create a stream
-			std::string blockName;
-			while (std::getline(blockStream, blockName, ';')) {
-				user->blocked_names.push_back(blockName);
-			}
-			allUsers.push_back(user);
-		}
-		file.close();
-	}
-	void regist(int fd, string username, string password) {
-		User *user1 = findUser(username);
-		if (user1 != nullptr ){
-			writeLine(fd, "This name has been registed, please user another name");
-			return;
-		}
-		User *user = new User(username, password);
-		for (int i=0; i<=1024; i++){
-			bool ava = true;
-			for (User *u : allUsers) {
-				if (u->id == i) {
-					ava = false;
-				}
-			}
-			if (ava) {
-				user->id = i;
-				break;
-			}
-		}
-		allUsers.push_back(user);
-		System::save();
-		writeLine(fd, "registed sucessful");
-	}
-	void save(){
-	}
-	void stats(int fd, string name){
-		char statsStr[1024];
-		User *user = findUser(name);
-		if (user == nullptr){
-			writeLine(fd,"User does not exist.");
-			return;
-		}
-		int i=0;
-		const char* boolStr1 = user->quiet ? "Yes" : "No";
-		const char* boolStr2 = user->login ? "Online" : "Offline";
-		string blockStr = "";
-		char loginStr[100] = "";
-		sprintf(loginStr,"%s is currently %s.",user->username.c_str(),boolStr2);
-		for ( i = 0; i < user->blocked_names.size(); i++) {
-			blockStr += user->blocked_names[i];
-			blockStr += " ";
-		}
-		sprintf(statsStr,"User: %s\nInfo: %s\nRating: %f\nWins: %d, Loses: %d\nQuiet: %s\nBlocked users: %s\n\n%s\n",
-			user->username.c_str(),user->information.c_str(),user->rating,user->win,user->loss,boolStr1,blockStr.c_str(),loginStr);
-		write(fd,statsStr,strlen(statsStr));
-	}
-};
+//     }
+// 	User() {
+//         this->username = username;
+//         this->password = password;
+//         information = username;
+//         quiet = false;
+//         login = false;
+//         sockId = -1;
+// 		cmd = 0;
+// 		win = 0;
+// 		loss = 0;
+// 		rating = 0;
+//     }
+// 	void logout() {
+// 		login = false;
+// 		cmd = 0;
+// 		sockId = -1;
+// 		//game-related should be dealed here
+// 	}
+// 	void writef(string buf) {
+// 		char outbuf[1024];
+// 		sprintf(outbuf, "<%s: %i> %s", username.c_str(), cmd, buf.c_str());
+// 		cmd++;
+// 		// Ensure num is the length of outbuf, not the result of the read call
+// 		write(sockId, outbuf, strlen(outbuf)); 
+// 	}
+// };
+// struct System {
+//     vector<User*> allUsers;
+// 	vector<User*> onlineUsers;
+// 	vector<string> getAllUsers() {
+//         vector<string> result;
+//         for (User *u : allUsers) {
+//             result.push_back(u->username);
+//         }
+//         return result;
+//     }
+// 	User* findUser(string username) {
+// 		rtrim(username);
+//         for (User *u : allUsers) {
+//             if (u->username == username) {
+// 				cout << "finded\n";
+//                 return u;
+//             }
+//         }
+//         return nullptr;
+//     }
+// 	User* findUserFd(const int fd) {
+//         for (User *u : allUsers) {
+//             if (u->sockId == fd) {
+//                 return u;
+//             }
+//         }
+//         return nullptr;
+//     }
+// 	void init() {
+// 		System::load_user();
+// 	}
+// 	void onlineUpdate(){
+// 		onlineUsers.clear();
+// 		for (User* u : allUsers) {
+// 			if (u->login) { // If the user is marked as online
+// 				onlineUsers.push_back(u);
+// 			}
+// 		}
+// 	}
+// 	void who(int fd) {
+// 		onlineUpdate();
+// 		char out1[100];
+// 		sprintf(out1,"Total %i user(s) online:\n", onlineUsers.size());
+// 		string out2;
+// 		for (User* u : onlineUsers) {
+// 			out2 += u->username;
+// 			out2 += " ";
+// 		}
+// 		write(fd, out1, strlen(out1));
+// 		writeLine(fd, out2);
+// 	}
+// 	void load_user() {
+// 		std::string filePath = "data/user"; // Adjust the path as necessary
+// 		std::ifstream file(filePath);
+// 		if (!file.is_open()) {
+// 			std::cerr << "Failed to open user file." << std::endl;
+// 		}
+// 		std::string line;
+// 		std::getline(file, line);
+// 		while (std::getline(file, line)) {
+// 			// user,password,id,info,rating,win,loses,quiet,block,online
+// 			int count = 0;
+// 			User *user = new User();
+// 			std::stringstream ss(line);
+// 			std::string item;
+// 			std::vector<std::string> tokens;
+// 			while (std::getline(ss, item, ',')) {
+// 				tokens.push_back(item);
+// 			}
+// 			user->username = tokens[0];
+// 			user->password = tokens[1];
+// 			user->id = std::stoi(tokens[2]);
+// 			user->information = tokens[3];
+// 			user->rating = std::stof(tokens[4]);
+// 			user->win = std::stoi(tokens[5]);
+// 			user->loss = std::stoi(tokens[6]);
+// 			user->quiet = std::stoi(tokens[7]);		
+// 			std::istringstream blockStream(tokens[8]); // Use tokens[8] to create a stream
+// 			std::string blockName;
+// 			while (std::getline(blockStream, blockName, ';')) {
+// 				user->blocked_names.push_back(blockName);
+// 			}
+// 			allUsers.push_back(user);
+// 		}
+// 		file.close();
+// 	}
+// 	void regist(int fd, string username, string password) {
+// 		User *user1 = findUser(username);
+// 		if (user1 != nullptr ){
+// 			writeLine(fd, "This name has been registed, please user another name");
+// 			return;
+// 		}
+// 		User *user = new User(username, password);
+// 		for (int i=0; i<=1024; i++){
+// 			bool ava = true;
+// 			for (User *u : allUsers) {
+// 				if (u->id == i) {
+// 					ava = false;
+// 				}
+// 			}
+// 			if (ava) {
+// 				user->id = i;
+// 				break;
+// 			}
+// 		}
+// 		allUsers.push_back(user);
+// 		System::save();
+// 		writeLine(fd, "registed sucessful");
+// 	}
+// 	void save(){
+// 	}
+// 	void stats(int fd, string name){
+// 		char statsStr[1024];
+// 		User *user = findUser(name);
+// 		if (user == nullptr){
+// 			writeLine(fd,"User does not exist.");
+// 			return;
+// 		}
+// 		int i=0;
+// 		const char* boolStr1 = user->quiet ? "Yes" : "No";
+// 		const char* boolStr2 = user->login ? "Online" : "Offline";
+// 		string blockStr = "";
+// 		char loginStr[100] = "";
+// 		sprintf(loginStr,"%s is currently %s.",user->username.c_str(),boolStr2);
+// 		for ( i = 0; i < user->blocked_names.size(); i++) {
+// 			blockStr += user->blocked_names[i];
+// 			blockStr += " ";
+// 		}
+// 		sprintf(statsStr,"User: %s\nInfo: %s\nRating: %f\nWins: %d, Loses: %d\nQuiet: %s\nBlocked users: %s\n\n%s\n",
+// 			user->username.c_str(),user->information.c_str(),user->rating,user->win,user->loss,boolStr1,blockStr.c_str(),loginStr);
+// 		write(fd,statsStr,strlen(statsStr));
+// 	}
+// };
 System sys;
 
 
 void login(int rec_sock, string username, string password) {
 	char buf[1024] = "username(guest):";
-	rtrim(password);
+	sys.rtrim(username);
+	sys.rtrim(password);
 	// string dusername = username.substr(0, username.length() - 2);
 	// string dpassword = password.substr(0, password.length() - 2);
 	User *user = sys.findUser(username);
 	if (user == nullptr) {
-		writeLine(rec_sock, "Incorrect username");
+		sys.writeLine(rec_sock, "Incorrect username");
 		states[rec_sock] = 0;
 		write(rec_sock, buf, strlen(buf));
 	} else if (user->password==password){
@@ -485,6 +488,3 @@ int main(int argc, char * argv[])
 	// int a;
 	// printf("%i\n",a);
 }
-
-
-
