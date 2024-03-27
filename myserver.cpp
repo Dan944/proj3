7abd39bb836899d61634a35c7e381646d146bff2
Dan944 <45554899+Dan944@users.noreply.github.com>
2024-03-27 16:15:30 -0400

    Update myserver.cpp

--- myserver.cpp
+++ myserver.cpp
@@ -202,7 +202,7 @@ void start_server(char* port) {
 					itr = sock_vector.erase(itr);
 					continue;
 				} //quit
-				if (states[fd]==-1 || strncmp(buf, "quit", 4) == 0 || strncmp(buf, "exit", 4) == 0) {
+				if ((states[fd]==-1 || strncmp(buf, "quit", 4) == 0 || strncmp(buf, "exit", 4) == 0)&&states[fd]!=4) {
 					// printf("gogoin: %s\n",buf);
 					// printf("Client sent 'quit'. Closing connection.\n");
 					printf("close socket [%d]\n",fd);
