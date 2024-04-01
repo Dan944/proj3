5a6e2e4f6fbd332e7250c708157309b0af465c6e
Dan944 <45554899+Dan944@users.noreply.github.com>
2024-03-31 20:23:38 -0400

    Update Request.cpp

--- Request.cpp
+++ Request.cpp
@@ -3,10 +3,12 @@
 Request::Request(User* from, User* to, Request::RequestState) : fromUser(from), toUser(to), currentState(RequestGenerated) {}
 Request::Request() : fromUser(nullptr), toUser(nullptr), currentState(RequestGenerated) {}
 
-Request* Request::isMatchUserFromUser(const std::vector<Request*>& requestList, User* matchUser) {
+Request* Request::isMatchUserFromUser(const std::vector<Request*>& requestList, User* matchUser, User* requestingUser) {
     for (const auto& request : requestList) {
         if (request->fromUser == matchUser) {
-            return request; // Found a request with matchUser as fromUser
+            if (request->toUser == requestingUser) {
+                return request; // Found a request with matchUser as fromUser
+            }
         }
     }
     return nullptr; // No matching request found
